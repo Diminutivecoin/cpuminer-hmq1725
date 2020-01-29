8b11367484d2b42b481610034d41d7b7fc999bd4
DiminutiveDev <43453576+Diminutivecoin@users.noreply.github.com>
2020-01-29 19:33:01 +0200

    Update cpu-miner.c

--- cpu-miner.c
+++ cpu-miner.c
@@ -3,7 +3,7 @@
  * Copyright 2012-2014 pooler
  * Copyright 2014 Lucas Jones
  * Copyright 2014 Tanguy Pruvot
- *
+ * Copyright 2020 Tanguy Pruvot
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation; either version 2 of the License, or (at your option)
